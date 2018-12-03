9741ac0579300e546238caf4b76653dd7da89acd
Guilherme Perinazzo <gperinazzo@gmail.com>
2018-12-03 15:53:06 +0000

    Change logic to find overlapped claims

--- puzzle-3/src/main.rs
+++ puzzle-3/src/main.rs
@@ -1,60 +1,43 @@
 #[macro_use]
 extern crate text_io;
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::io::{stdin, BufRead, BufReader};
 
 type Position = (u32, u32);
 type FabricMap = HashMap<Position, Vec<u32>>;
-struct Claim {
-    id: u32,
-    x: u32,
-    y: u32,
-    width: u32,
-    height: u32,
-}
 
 fn main() {
     let input = BufReader::new(stdin());
     let mut fabric = FabricMap::new();
-    let mut claims: Vec<Claim> = vec![];
+    let mut overlapped_claims: HashMap<u32, bool> = HashMap::new();
     for line in input.lines().filter_map(Result::ok) {
+        let mut colliding_ids: HashSet<u32> = HashSet::new();
         let (id, x, y, width, height): (u32, u32, u32, u32, u32);
         scan!(line.bytes() => "#{} @ {},{}: {}x{}", id, x, y, width, height);
-        claims.push(Claim {
-            id,
-            x,
-            y,
-            width,
-            height,
-        });
         for i in (x + 1)..=(x + width) {
             for j in (y + 1)..=(y + height) {
                 let pos = (i, j);
                 fabric
                     .entry(pos)
-                    .and_modify(|v| v.push(id))
+                    .and_modify(|v| {
+                        colliding_ids.extend(v.iter());
+                        v.push(id)
+                    })
                     .or_insert(vec![id]);
             }
+        }
+        overlapped_claims.insert(id, !colliding_ids.is_empty());
+        for id in colliding_ids.drain() {
+            let entry = overlapped_claims.entry(id).or_default();
+            *entry = true;
         }
     }
     let result: usize = fabric.values().map(|v| v.len()).filter(|l| *l > 1).count();
     println!("{}", result);
-    for claim in claims {
-        let mut overlaps = false;
-        for i in claim.x + 1..=claim.x + claim.width {
-            for j in claim.y + 1..=claim.y + claim.height {
-                match fabric.get(&(i, j)) {
-                    Some(v) if v.len() > 1 => {
-                        overlaps = true;
-                        break;
-                    }
-                    _ => (),
-                };
-            }
-        }
-        if !overlaps {
-            println!("{}", claim.id);
+    for (id, overlapped) in overlapped_claims.iter() {
+        if !overlapped {
+            println!("{}", id);
             break;
         }
     }
